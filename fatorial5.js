fa6bc70a9a33194e9339744d670952de4054ae76
projeto 1 <129951535+matheusinho21@users.noreply.github.com>
2024-04-23 10:24:25 -0300

    Create fatorial5.js

---
+++ fatorial5.js
@@ -0,0 +1,12 @@
+const fatorial = 30;
+let i = 0;
+let resultado = 0;
+
+resultado = fatorial;
+console.log(resultado + "!");
+for (i = 1; i < fatorial; i++ ) {
+    
+    console.log(resultado + " = " +resultado+" * ("+
+                fatorial+ " - " + i + ")");
+    resultado =resultado * (fatorial - i);
+}
