5333719ac998257296e466c6d05dec53238c3035
projeto 1 <129951535+matheusinho21@users.noreply.github.com>
2024-04-23 09:19:36 -0300

    Create fatorial3.js

---
+++ fatorial3.js
@@ -0,0 +1,4 @@
+const fatorial = 10; 
+let resultado = 0;
+resultado = fatorial * (fatorial-1) * (fatorial-2) * (fatorial-3) * (fatorial-4) * (fatorial-5) * (fatorial-6) * (fatorial-7) * (fatorial-8) * (fatorial-9)
+console.log("o resultado é :" +resultado);
